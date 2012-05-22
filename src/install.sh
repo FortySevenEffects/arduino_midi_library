4f8b37747103db42670c2d7f1e542571f95e9275
Francois Best <francoisbest@gmail.com>
2012-05-22 21:51:45 +0200

    Added scripts.

---
+++ src/install.sh
@@ -0,0 +1,3 @@
+cp ./MIDI.* /Applications/Arduino.app/Contents/Resources/Java/libraries/MIDI 
+cd teensy_core/usb_midi
+./install.sh
---
+++ teensy_core/usb_midi/install.sh
@@ -0,0 +1,3 @@
+cp *.cpp /Applications/Arduino.app/Contents/Resources/Java/hardware/teensy/cores/usb_midi/
+cp *.c /Applications/Arduino.app/Contents/Resources/Java/hardware/teensy/cores/usb_midi/
+cp *.h /Applications/Arduino.app/Contents/Resources/Java/hardware/teensy/cores/usb_midi/
