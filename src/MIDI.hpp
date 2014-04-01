240cd22021f98302167a1b206b8a27d5d22b310e
Francois Best <francoisbest@gmail.com>
2014-04-01 09:03:44 +0200

    Added explicit license header and starting 4.1 branch.

--- src/MIDI.hpp
+++ src/MIDI.hpp
@@ -2,10 +2,23 @@
  *  @file       midi_Inline.hpp
  *  Project     Arduino MIDI Library
  *  @brief      MIDI Library for the Arduino - Inline implementations
- *  @version    4.0
+ *  @version    4.1
  *  @author     Francois Best
  *  @date       24/02/11
- *  license     GPL Forty Seven Effects - 2011
+ *  @license    GPL v3.0 - Copyright Forty Seven Effects 2014
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #pragma once
