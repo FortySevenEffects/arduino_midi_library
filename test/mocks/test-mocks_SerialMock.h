faa7bade0b96773dda332052d96cc7025e757f47
Francois Best <francois@francoisbest.com>
2018-11-07 12:08:12 +0100

    cleanup: Dedupe RingBuffer implementation in mocks

--- test/mocks/test-mocks_SerialMock.h
+++ test/mocks/test-mocks_SerialMock.h
@@ -1,38 +1,10 @@
 #pragma once
 
 #include "test-mocks.h"
-#include "test-mocks_Defs.h"
+#include <inttypes.h>
+#include <src/midi_RingBuffer.h>
 
 BEGIN_TEST_MOCKS_NAMESPACE
-
-template<typename DataType, int Size>
-class RingBuffer
-{
-public:
-     RingBuffer();
-    ~RingBuffer();
-
-public:
-    int getLength() const;
-    bool isEmpty() const;
-
-public:
-    void write(DataType inData);
-    void write(const DataType* inData, int inSize);
-    void clear();
-
-public:
-    DataType peek() const;
-    DataType read();
-    void read(DataType* outData, int inSize);
-
-private:
-    DataType mData[Size];
-    DataType* mWriteHead;
-    DataType* mReadHead;
-};
-
-// -----------------------------------------------------------------------------
 
 template<int BufferSize>
 class SerialMock
@@ -44,14 +16,14 @@ public:
 public: // Arduino Serial API
     void begin(int inBaudrate);
     int available() const;
-    void write(uint8 inData);
-    uint8 read();
+    void write(uint8_t inData);
+    uint8_t read();
 
 public: // Test Helpers API
     void moveTxToRx(); // Simulate loopback
 
 public:
-    typedef RingBuffer<uint8, BufferSize> Buffer;
+    typedef midi::RingBuffer<uint8_t, BufferSize> Buffer;
     Buffer mTxBuffer;
     Buffer mRxBuffer;
     int mBaudrate;
