3db2424fc9eae721327c260b390813b95151a626
lathoub <bart.de.lathouwer@telenet.be>
2020-03-22 17:26:22 +0100

    cleanup

--- examples/ErrorCallback/ErrorCallback.ino
+++ examples/ErrorCallback/ErrorCallback.ino
@@ -1,28 +1,17 @@
 #include <MIDI.h>
 
-// Simple tutorial on how to receive and send MIDI messages.
-// Here, when receiving any message on channel 4, the Arduino
-// will blink a led and play back a note for 1 second.
+// Before running the program below, make sure you set
+// UseReceiverActiveSensing (optionally UseSenderActiveSensing) in Settings.h to true
 
 MIDI_CREATE_DEFAULT_INSTANCE();
-USING_NAMESPACE_MIDI
 
 void handleError(int8_t err)
 {
-  if (bitRead(err, ErrorActiveSensingTimeout))
-    Serial.println("ActiveSensing Timeout");
-  else
-    Serial.println("ActiveSensing OK");
-
   digitalWrite(LED_BUILTIN, (err == 0)? LOW : HIGH);
 }
 
 void setup()
 {
-  Serial.begin(115200);
-  while (!Serial) {}
-  Serial.println("booting");
-
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
 
