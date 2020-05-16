daeb810c2c73a2ff1a30fae0f3cca56be92c4f62
lathoub <lathoub@gmail.com>
2020-05-16 11:16:46 +0200

    fix bug in MIDI_CREATE_CUSTOM_INSTANCE
    - MIDI Setting in MIDI_CREATE_CUSTOM_INSTANCE, but Setting for SerialMIDI
    - MACRO outside of Namespace

--- src/serialMIDI.h
+++ src/serialMIDI.h
@@ -91,6 +91,8 @@ private:
     SerialPort& mSerial;
 };
 
+END_MIDI_NAMESPACE
+
 /*! \brief Create an instance of the library attached to a serial port.
  You can use HardwareSerial or SoftwareSerial for the serial port.
  Example: MIDI_CREATE_INSTANCE(HardwareSerial, Serial2, midi2);
@@ -119,7 +121,5 @@ private:
  @see MIDI_CREATE_INSTANCE
  */
 #define MIDI_CREATE_CUSTOM_INSTANCE(Type, SerialPort, Name, Settings)           \
-    MIDI_NAMESPACE::SerialMIDI<Type, Settings> serial##Name(SerialPort);\
-    MIDI_NAMESPACE::MidiInterface<MIDI_NAMESPACE::SerialMIDI<Type, Settings>> Name((MIDI_NAMESPACE::SerialMIDI<Type, Settings>&)serial##Name);
-
-END_MIDI_NAMESPACE
+    MIDI_NAMESPACE::SerialMIDI<Type> serial##Name(SerialPort);\
+    MIDI_NAMESPACE::MidiInterface<MIDI_NAMESPACE::SerialMIDI<Type>, Settings> Name((MIDI_NAMESPACE::SerialMIDI<Type>&)serial##Name);
