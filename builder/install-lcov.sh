9c9c601cf39db4f5ff0df127aa2e7d71a5083f15
Francois Best <francois@francoisbest.com>
2021-08-10 09:13:27 +0200

    chore: Inline install script (reduce indirections)

--- builder/install-lcov.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-export LCOV_ROOT="$HOME/lcov"
-mkdir -p "$LCOV_ROOT"
-wget http://ftp.de.debian.org/debian/pool/main/l/lcov/lcov_1.14.orig.tar.gz --output-document="$LCOV_ROOT/lcov.tar.gz"
-tar xf "$LCOV_ROOT/lcov.tar.gz" --strip-components=1 -C $LCOV_ROOT
-export PATH="$LCOV_ROOT/bin:$PATH"
-which lcov
